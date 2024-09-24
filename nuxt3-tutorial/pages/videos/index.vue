75dd52af9cb8b6214f3cd2f5235713fc22378489
80815723 <80815723@telefonicati.onmicrosoft.com>
2024-09-24 02:36:43 -0300

    Nuxt 3 - NuxtUI - Videos

--- nuxt3-tutorial/pages/videos/index.vue
+++ nuxt3-tutorial/pages/videos/index.vue
@@ -1,19 +1,41 @@
 <template>
     <div>
-        <h1>Vídeos</h1>
+        <h1 class="text-4xl text-center my-5">{{ $t("titulo") }}</h1>
     </div>
-    <h1>{{ $t("titulo") }}</h1>
-    <nuxt-link to="/videos/favoritos">Favoritos</nuxt-link>
-    <div class="videos">
-        <div v-for="video in videos" :key="video.id">
-            <h2>{{ video.descricao }}</h2>
-            <p v-data-horario='"dd / mm / yyyy"'>{{ video.data_postagem }}</p>
-            <iframe width="500" height="400" :src="video.url" title="Youtube video player" frameborder="0" />
 
-            <div>
-                <button @click="adicionarFavorito(video)">Adicionar Favorito</button>
-            </div>
-        </div>
+    <div class="grid grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4">
+        <UCard v-for="video in videos" :key="video.id">
+            <template #header>
+                <div class="flex justify-between items-center">
+                    <h2>{{ video.descricao }}</h2>
+                    <UBadge color="black" variant="solid" size="xs" v-data-horario='"dd / mm / yyyy"'>{{
+                        video.data_postagem }}</UBadge>
+                </div>
+            </template>
+
+            <iframe class="h-48 w-full" :src="video.url" title="Youtube video player" frameborder="0" />
+
+            <template #footer>
+                <div class="flex justify-between items-center">
+                    <NuxtLink :to="{
+                        name: 'videos-id',
+                        params: { id: video.id.toString() }
+                    }">
+                        <UButton label="Ver video" color="gray">
+                            <template #trailing>
+                                <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
+                            </template>
+                        </UButton>
+                    </NuxtLink>
+
+                    <UButton color="white" variant="link" @click="favoritar(video)">
+                        <template #trailing>
+                            <UIcon name="i-heroicons:heart" class="w-5 h-5" />
+                        </template>
+                    </UButton>
+                </div>
+            </template>
+        </UCard>
     </div>
 </template>
 
@@ -24,7 +46,7 @@ import { useVideoStore } from '~/stores/video'
 const { $toast } = useNuxtApp()
 
 onMounted(() => {
-    $toast.success('Toast adicionado com sucesso')
+
 })
 
 const { adicionarFavorito } = useVideoStore()
@@ -56,30 +78,9 @@ const videos: Video[] = [
     },
 ]
 
+const favoritar = (video: Video) => {
+    adicionarFavorito(video)
+    $toast.success('Toast adicionado com sucesso')
+}
+
 </script>
-
-<style scoped>
-.videos {
-    max-width: 1200px;
-    margin: 0 auto;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
-    justify-content: space-around;
-}
-
-.videos button {
-    display: inline-block;
-    margin-top: 5px;
-    color: white;
-    padding: 15px;
-    border: none;
-    border-radius: 5px;
-    background: #00CF7B;
-    cursor: pointer
-}
-
-.videos button:hover {
-    background: #008550;
-}
-</style>
