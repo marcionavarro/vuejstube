d0c3f5cdafa794a09dfeba7b1fc2cfb5d7cb21f2
80815723 <80815723@telefonicati.onmicrosoft.com>
2024-10-11 14:34:32 -0300

    Nuxt 3 - Footer fixed

--- nuxt3-tutorial/layouts/default.vue
+++ nuxt3-tutorial/layouts/default.vue
@@ -9,62 +9,32 @@
       <div class="block lg:hidden">
         <button
           class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-          @click="toggleMenu"
-        >
-          <svg
-            class="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          @click="toggleMenu">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
-            <path
-              :class="{ hidden: isOpen, 'inline-block': !isOpen }"
-              class="fill-current"
-              d="M0 0h20v20H0z"
-            />
-            <path
-              :class="{ hidden: !isOpen, 'inline-block': isOpen }"
-              class="fill-current"
-              d="M0 0h20v20H0z"
-            />
+            <path :class="{ hidden: isOpen, 'inline-block': !isOpen }" class="fill-current" d="M0 0h20v20H0z" />
+            <path :class="{ hidden: !isOpen, 'inline-block': isOpen }" class="fill-current" d="M0 0h20v20H0z" />
           </svg>
         </button>
       </div>
-      <div
-        :class="{ hidden: !isOpen, block: isOpen }"
-        class="w-full block flex-grow lg:flex lg:items-center lg:w-auto"
-      >
+      <div :class="{ hidden: !isOpen, block: isOpen }" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-          <NuxtLink
-            v-for="rota in rotasSistemas()"
-            :key="rota.nome"
-            :to="rota.path"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
+          <NuxtLink v-for="rota in rotasSistemas()" :key="rota.nome" :to="rota.path"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             {{ rota.nome }}
           </NuxtLink>
         </div>
         <div>
           <ClientOnly>
-            <UButton
-              :icon="
-                isDark
-                ? 'i-heroicons-moon-20-solid'
-                : 'i-heroicons-sun-20-solid'
-              "
-              color="gray"
-              variant="ghost"
-              aria-label="Theme"
-              @click="isDark = !isDark"
-            />
+            <UButton :icon="isDark
+              ? 'i-heroicons-moon-20-solid'
+              : 'i-heroicons-sun-20-solid'
+              " color="gray" variant="ghost" aria-label="Theme" @click="isDark = !isDark" />
             <template #fallback>
               <div class="w-8 h-8" />
             </template>
           </ClientOnly>
-          <select
-            v-model="locale"
-            class="ml-8"
-          >
+          <select v-model="locale" class="ml-8">
             <option value="pt">pt</option>
             <option value="en">en</option>
           </select>
@@ -74,16 +44,15 @@
     <UContainer class="my-10">
       <slot></slot>
     </UContainer>
-    <UDivider
-      label="Marcio Navarro Fullstack Developer"
-      :ui="{ label: 'text-primary-500 dark:text-primary-400' }"
-    />
 
-    Rodapé Padrão
+
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
+import Footer from '~/components/Footer.vue';
+
 const { locale } = useI18n();
 const isOpen = ref(false);
 const colorMode = useColorMode();
