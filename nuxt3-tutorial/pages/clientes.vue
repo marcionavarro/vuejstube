ce4bf133e3e5e681f4de3cbd961cc8912214fc65
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-10-22 01:51:42 -0300

    Nuxt 3 - Refatoração do design da pagina clientes e paginação

--- nuxt3-tutorial/pages/clientes.vue
+++ nuxt3-tutorial/pages/clientes.vue
@@ -21,39 +21,51 @@
     </button>
     <div v-if="status === 'pending'">{{ t("carregando") }}...</div>
     <div v-else-if="error">{{ error.message }}</div>
-    <div
-      v-else
-      class="grid md:grid-cols-3 sm:grid-cols-2 gap-4"
-    >
+
+    <div class="grid md:grid-cols-3 sm:grid-cols-2">
       <div
-        v-for="cliente in clientes"
+        v-for="cliente in clientes.clientes"
         :key="cliente.id"
-        class="shadow-md px-4 py-2"
+        class="card-client"
       >
-        <img
-          :src="cliente.foto"
-          class="rounded-full mx-auto"
-        />
-        <h3 class="text-2xl text-center">{{ cliente.nome }}</h3>
+        <div class="user-picture">
+          <img
+            :src="cliente.foto"
+            :alt="cliente.nome"
+          >
+        </div>
+        <p class="name-client"> {{ cliente.nome }} {{ cliente.sobrenome }}
+          <span>{{ cliente.email }}</span>
+        </p>
       </div>
     </div>
 
-    <div class="flex gap-2">
+    <div class="pagination">
       <button
-        v-for="i in 2"
-        :key="n"
-        class="px-2 bg-slate-400"
-        @click="irPagina(i)"
+        @click="fetchPreviousPage"
+        :disabled="pagina === 1"
+        class="pagination-button"
       >
-        {{ i }}
+        Anterior
+      </button>
+      <span class="pagination-text">
+        Página {{ pagina }} de {{ totalPages }}
+      </span>
+      <button
+        @click="fetchNextPage"
+        :disabled="pagina >= totalPages"
+        class="pagination-button"
+      >
+        Próxima
       </button>
     </div>
   </div>
 </template>
 
 <script setup>
-const pagina = ref(1)
 const { t } = useI18n();
+const pagina = ref(1)
+const limit = ref(6)
 
 definePageMeta({
   pageTransition: {
@@ -64,36 +76,123 @@ definePageMeta({
 const reqres = `https://reqres.in/api/users`
 
 const { data: clientes, error, refresh, execute, status } = useLazyFetch(reqres, {
-  //server: false
-  //pick: ['page', 'data']
-  transform: (clientes) => {
-    return clientes.data.map(c => ({
-      id: c.id,
-      nome: c.first_name,
-      sobrenome: c.last_name,
-      foto: c.avatar
-    }))
+  transform: (response) => {
+    return {
+      clientes: response.data.map(c => ({
+        id: c.id,
+        email: c.email,
+        nome: c.first_name,
+        sobrenome: c.last_name,
+        foto: c.avatar
+      })),
+      total: response.total,
+      totalPages: response.total_pages
+    }
   },
-  //watch: [pagina],
   query: {
-    page: pagina
+    page: pagina,
+    per_page: limit
   }
-});
-
-/* const reqresUnknown = `https://reqres.in/api/unknown`
-const { data: cliente } = await useAsyncData('cliente', async () => {
-    const [pessoas, aleatorios] = await Promise.all([
-        $fetch(reqres),
-        $fetch(reqresUnknown)
-    ])
-
-    return { pessoas, aleatorios }
 })
 
-console.log('cliente pessoas:: ', cliente.value.pessoas)
-console.log('cliente aleatorios:: ', cliente.value.aleatorios) */
-
-const irPagina = (numeroPagina) => {
-  pagina.value = numeroPagina;
+function fetchNextPage() {
+  if (pagina.value < totalPages.value) {
+    pagina.value++
+  }
 }
+
+function fetchPreviousPage() {
+  if (pagina.value > 1) {
+    pagina.value--
+  }
+}
+
+const totalPages = computed(() => {
+  return clientes.value ? clientes.value.totalPages : 1
+})
 </script>
+
+<style scoped>
+.card-client {
+  background: #2cb5a0;
+  width: 25rem;
+  margin: 15px 0;
+  padding: 10px 0;
+
+  border: 4px solid #7cdacc;
+  box-shadow: 0 6px 10px rgba(207, 212, 222, 1);
+  border-radius: 10px;
+  text-align: center;
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+  transition: all 0.3s ease;
+}
+
+.card-client:hover {
+  transform: translateY(-10px);
+}
+
+.user-picture {
+  overflow: hidden;
+  object-fit: cover;
+  width: 10rem;
+  height: 10rem;
+  border: 9px solid #7cdacc;
+  border-radius: 999px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+}
+
+.user-picture img {
+  width: 10rem;
+  height: 100%;
+}
+
+.name-client {
+  margin: 0;
+  margin-top: 20px;
+  font-weight: 600;
+  font-size: 18px;
+}
+
+.name-client span {
+  display: block;
+  font-weight: 200;
+  font-size: 16px;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  gap: 10px;
+}
+
+.pagination-button {
+  background-color: #14b8a6; /* Cor principal */
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background-color 0.3s ease;
+}
+
+.pagination-button:hover {
+  background-color: #0f766e; /* Tom mais escuro ao passar o mouse */
+}
+
+.pagination-button:disabled {
+  background-color: #94d6d2; /* Cor mais clara quando desabilitado */
+  cursor: not-allowed;
+}
+
+.pagination-text {
+  font-size: 14px;
+  color: #333;
+}
+</style>
