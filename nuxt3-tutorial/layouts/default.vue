73ae605ee703d11ef644ed9709a8939faad1b81a
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-11-20 02:03:17 -0300

    Nuxt 3 - Pagina inicial e ajustes

--- nuxt3-tutorial/layouts/default.vue
+++ nuxt3-tutorial/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="wrapper">
+    <nav class="flex items-center justify-between flex-wrap bg-[#00c16a] p-6">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
         <NuxtLink to="/">
           <span class="font-semibold text-xl tracking-tight">Marcio Navarro</span>
@@ -9,37 +9,72 @@
       <div class="block lg:hidden">
         <button
           class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-          @click="toggleMenu">
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          @click="toggleMenu"
+        >
+          <svg
+            class="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <title>Menu</title>
-            <path :class="{ hidden: isOpen, 'inline-block': !isOpen }" class="fill-current" d="M0 0h20v20H0z" />
-            <path :class="{ hidden: !isOpen, 'inline-block': isOpen }" class="fill-current" d="M0 0h20v20H0z" />
+            <path
+              :class="{ hidden: isOpen, 'inline-block': !isOpen }"
+              class="fill-current"
+              d="M0 0h20v20H0z"
+            />
+            <path
+              :class="{ hidden: !isOpen, 'inline-block': isOpen }"
+              class="fill-current"
+              d="M0 0h20v20H0z"
+            />
           </svg>
         </button>
       </div>
-      <div :class="{ hidden: !isOpen, block: isOpen }" class="w-full block lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm" v-for="rota in rotasSistemasDefault()" :key="rota.nome">
-          <NuxtLink  v-if="!rota.permissao || rota.permissao === user?.permissao" :to="rota.path"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <div
+        :class="{ hidden: !isOpen, block: isOpen }"
+        class="w-full block lg:flex lg:items-center lg:w-auto"
+      >
+        <div
+          class="text-sm"
+          v-for="rota in rotasSistemasDefault()"
+          :key="rota.nome"
+        >
+          <NuxtLink
+            v-if="!rota.permissao || rota.permissao === user?.permissao"
+            :to="rota.path"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
             {{ t(`menu.${rota.nome}`) }}
           </NuxtLink>
         </div>
         <div class="flex items-center gap-5">
           <ClientOnly>
-            <UButton :icon="isDark
+            <UButton
+              :icon="isDark
               ? 'i-heroicons-moon-20-solid'
               : 'i-heroicons-sun-20-solid'
-              " color="gray" variant="ghost" aria-label="Theme" @click="isDark = !isDark" />
+              "
+              color="gray"
+              variant="ghost"
+              aria-label="Theme"
+              @click="isDark = !isDark"
+            />
             <template #fallback>
               <div class="w-8 h-8" />
             </template>
           </ClientOnly>
 
-          <USelect v-model="locale" icon="i-heroicons:globe-alt-solid" color="white" size="sm" :options="options"
-            placeholder="Tradutor..." />
-            <ClientOnly v-if="loggedIn">
-              <LayoutDropdowUsuario />
-            </ClientOnly>
+          <USelect
+            v-model="locale"
+            icon="i-heroicons:globe-alt-solid"
+            color="white"
+            size="sm"
+            :options="options"
+            placeholder="Tradutor..."
+          />
+          <ClientOnly v-if="loggedIn">
+            <LayoutDropdowUsuario />
+          </ClientOnly>
         </div>
       </div>
     </nav>
@@ -52,8 +87,8 @@
 </template>
 
 <script setup lang="ts">
-import { rotasSistemas } from '#imports';
-import Footer from '~/components/Footer.vue';
+import { rotasSistemas } from "#imports";
+import Footer from "~/components/Footer.vue";
 
 const { locale, t } = useI18n();
 const colorMode = useColorMode();
@@ -62,9 +97,9 @@ const isOpen = ref(false);
 const { loggedIn, user } = useUserSession();
 
 const options = ref([
-  { label: 'Português', value: 'pt' },
-  { label: 'Inglês', value: 'en' }
-])
+  { label: "Português", value: "pt" },
+  { label: "Inglês", value: "en" },
+]);
 
 const toggleMenu = () => {
   isOpen.value = !isOpen.value;
@@ -78,5 +113,11 @@ const isDark = computed({
     colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
   },
 });
-
 </script>
+
+<style scoped>
+.wrapper {
+  max-width: 1777px;
+  margin: 0 auto;
+}
+</style>
--- nuxt3-tutorial/pages/index.vue
+++ nuxt3-tutorial/pages/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="mt-10 mb-20">
-    <Header />
-    <div>
-      <h1>Página Inicial</h1>
-      {{ user }}
+  <div class="page-mousemove flex flex-col items-center mt-40 mb-20">
+    <div class="w-1/2">
+      <IconLogo class="logo" />
+      <p class="font-sans text-3xl text-emerald-400 mt-4">web full-stack de alto desempenho</p>
     </div>
   </div>
 </template>
 <script setup>
-const { user } = useUserSession();
 const tituloSite = "Marcio Navarro - Developer"
 useHead({
   title: tituloSite,
@@ -18,5 +16,72 @@ useHead({
       content: "Progamador Fullstack",
     },
   ],
+  bodyAttrs: {
+    class: 'page-mousemove'
+  }
 });
+
+onMounted(() => {
+  document.addEventListener('mousemove', spark)
+  onUnmounted(() => {
+    document.removeEventListener('mousemove', spark)
+  })
+})
+
+const spark = (event) => {
+  let i = document.createElement('i')
+  i.style.left = event.pageX + 'px'
+  i.style.top = event.pageY + 'px'
+  i.style.scale = `${Math.random() * 2 + 1}`
+  i.style.setProperty('--x', getRandomTransitionValue())
+  i.style.setProperty('--y', getRandomTransitionValue())
+
+  document.body.appendChild(i)
+
+  setTimeout(() => {
+    document.body.removeChild(i);
+  }, 2000);
+}
+
+const getRandomTransitionValue = () => {
+  return `${Math.random() * 400 - 200}px`;
+}
 </script>
+
+<style>
+body.page-mousemove {
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+  background: #fff url("../assets/image/coffe.png") no-repeat bottom/cover;
+  background-size: auto;
+  background-position-y: calc(100% - 70px);
+  background-blend-mode: difference;
+}
+
+i {
+  position: absolute;
+  width: 4px;
+  height: 4px;
+  background: #00c16a;
+  pointer-events: none;
+  animation: animate 2s linear forwards;
+}
+
+.logo {
+  color: #00dc82;
+  width: 100%;
+  max-width: 600px;
+}
+
+@keyframes animate {
+  0% {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
+  100% {
+    opacity: 0;
+    transform: translate(var(--x), var(--y));
+  }
+}
+</style>
